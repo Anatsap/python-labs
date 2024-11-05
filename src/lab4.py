7ce2868ffcb516d2e6a4e058d9827bad4e1d60d0
Anastasiia Tsapiv <nasta.tsv@gmail.com>
2024-11-05 20:43:23 +0200

    Update __str__

--- src/lab4.py
+++ src/lab4.py
@@ -19,10 +19,14 @@ class WaterPump:
 
 
     def __str__(self)-> str :
-        return f"Power consumption = {self.__power_consumption},Brand = {self.__brand}, Volume per hour = {self.__volume_ph}, Price = {self.price}, Name = {self.name}"
+        return f"""Power consumption = {self.__power_consumption} W,
+Brand = {self.__brand},
+Volume per hour = {self.__volume_ph} L/h,
+Price = {self.price} $,
+Name = {self.name}"""
 
     def __repr__(self)-> str :
-        return f"WaterPump(power_consumption='{self.__power_consumption}', brand='{self.__brand}', volume_ph='{self.__volume_ph}', price='{self.price}', name='{self.name}')"
+        return f"WaterPump(power_consumption='{self.__power_consumption}', brand='{self.__brand}', volume_ph='{self.__volume_ph}, price='{self.price}', name='{self.name})"
 
 
     def __del__ (self):
@@ -35,7 +39,8 @@ def main():
     waterpump_samsung = WaterPump(500, "Samsung", 100, 4500.0, "Water pump FE23")
     waterpumps = [waterpump_default, waterpump_ultra, waterpump_samsung]
     for waterpump in waterpumps:
-        print(waterpumps)
+        print(waterpump)
+        print()
     
 
 if __name__ == "__main__":
